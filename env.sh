920f93ed784a32d5a0de1959590423e88fbc4ec0
getXzooted <getxzooted@gmail.com>
2025-12-23 17:56:17 -0500

    old onyx root without bash_source

--- env.sh
+++ env.sh
@@ -3,11 +3,11 @@
 # Purpose: Establishes paths and loads foundational libraries.
 
 # 1. Establish Root Context
-# CURRENT_SCRIPT_PATH="$(readlink -f "$0")"
-# export ONYX_ROOT="$(dirname "$(dirname "$CURRENT_SCRIPT_PATH")")"
+ CURRENT_SCRIPT_PATH="$(readlink -f "$0")"
+ export ONYX_ROOT="$(dirname "$(dirname "$CURRENT_SCRIPT_PATH")")"
 
-CURRENT_SCRIPT_PATH="$(readlink -f "${BASH_SOURCE[0]}")"
-export ONYX_ROOT="$(dirname "$(dirname "$CURRENT_SCRIPT_PATH")")"
+#CURRENT_SCRIPT_PATH="$(readlink -f "${BASH_SOURCE[0]}")"
+#export ONYX_ROOT="$(dirname "$(dirname "$CURRENT_SCRIPT_PATH")")"
 
 # 2. Fallback for standard installation
 [[ -z "$ONYX_ROOT" || "$ONYX_ROOT" == "/" ]] && export ONYX_ROOT="/opt/onyx"
