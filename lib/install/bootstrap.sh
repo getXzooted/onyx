4bdd027c3d29addc2a650fb6dd0709997898467f
getXzooted <getxzooted@gmail.com>
2025-12-29 17:55:51 -0500

    new future unified bootstrap controller

---
+++ lib/install/bootstrap.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# lib/install/bootstrap.sh
+
+function bootstrap() {
+    log_header "ARMING SOVEREIGN CONTROLLER"
+    
+    local SERVICE_FILE="/etc/systemd/system/onyx.service"
+    
+    log_step "Creating permanent /etc/systemd/system/onyx.service..."
+    cat <<EOF > "$SERVICE_FILE"
+[Unit]
+Description=Onyx Sovereign Gateway Controller
+After=network-online.target local-fs.target
+Wants=network-online.target
+
+[Service]
+Type=oneshot
+ExecStart=/usr/local/bin/onyx provision
+StandardOutput=inherit
+StandardError=inherit
+User=root
+RemainAfterExit=yes
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+    systemctl daemon-reload
+    systemctl enable onyx.service
+    log_success "Controller armed. Initial entry: 'onyx provision'"
+}
