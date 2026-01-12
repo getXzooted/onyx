d555b668fb4d0756ccc44eb2e3cb9c31d9a3ee98
getXzooted <getxzooted@gmail.com>
2026-01-12 15:20:18 -0500

    moved logger functions up

--- env.sh
+++ env.sh
@@ -12,14 +12,14 @@
 # 2. Fallback for standard installation
 [[ -z "$ONYX_ROOT" || "$ONYX_ROOT" == "/" ]] && export ONYX_ROOT="/opt/onyx"
 
-# 3. Discover Hardware and Enroll
+# 3. Load Global Foundation
+source "$ONYX_ROOT/core/functions.sh"
+source "$ONYX_ROOT/core/logger.sh"
+
+# 4. Discover Hardware and Enroll
 source "$ONYX_ROOT/modules/network/discovery.sh"
 discover_hardware # Finds WAN and populates interfaces in hardening.yml
 enroll_hardware # Enrolls the Hardware with default 'off'
 
-# 3. Initialize Variables
+# 5. Initialize Variables
 source "$ONYX_ROOT/vars.sh"
-
-# 4. Load Global Foundation
-source "$ONYX_ROOT/core/functions.sh"
-source "$ONYX_ROOT/core/logger.sh"
