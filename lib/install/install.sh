a52737e56dc0ad5035422bfb01cdd70149fce5a2
getXzooted <getxzooted@gmail.com>
2026-01-18 14:27:02 -0500

    changed to return inside conditional

--- lib/install/install.sh
+++ lib/install/install.sh
@@ -3,15 +3,18 @@
 # Purpose: Handles the installation of ONYX modules
 
 function onyx_install() {
+    
     # Check if the installation script exists
     local INSTALL_SCRIPT="$LIB_DIR/install/install.sh"
 
     if [ -f "$INSTALL_SCRIPT" ]; then
         log_header "--- STARTING INSTALLATION ---"
         source "$INSTALL_SCRIPT"
+        return 0
     else
         log_error "Install module not found at $INSTALL_SCRIPT"
         log_info "Please ensure lib/install/install.sh exists."
-        exit 1
+        return 1
     fi
+
 }
