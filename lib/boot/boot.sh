3270fd79c826677ea4625723031101de4f043f2c
getXzooted <getxzooted@gmail.com>
2026-01-18 14:23:13 -0500

    moved return inside conditional

--- lib/boot/boot.sh
+++ lib/boot/boot.sh
@@ -3,17 +3,18 @@
 # Purpose: Initializes Onyx during system boot.
 
 function onyx_boot() {
+    
     # Check if the boot script exists
     BOOT_SCRIPT="$MODULES_DIR/system/boot.sh"
 
     if [ -f "$BOOT_SCRIPT" ]; then
         log_header "--- BOOTSTRAPPING ONYX GATEWAY ---"
         source "$BOOT_SCRIPT"
+        return 0
     else
         log_error "Boot module not found at $BOOT_SCRIPT"
         log_info "Please ensure $MODULES_DIR/system/boot.sh exists."
         return 1
     fi
 
-    return 0
 }
